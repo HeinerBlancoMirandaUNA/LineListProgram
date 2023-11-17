2e5895420bb455f6229c226c6713dee7526519bf
HeinerBlancoMirandaUNA <heiner.blanco.miranda@est.una.ac.cr>
2023-11-17 03:52:36 -0600

    Improve UML diagram and add missing methods to DoublyLinkedlist
    	-go() can now be used to select an specific item using an
    	integer
    	-add() method can now be used to insert items in the
    	middle of the list
    	-Improve del() method

--- include/doubly_linked_list.h
+++ include/doubly_linked_list.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <cstdlib>
 #include "node.h"
 
 enum ListPosition {
@@ -17,6 +18,7 @@ private:
     Node<T>* current;
     T dummy;
     int listSize;
+    int currentPosition;
 
     void deleteOperation (){
 
@@ -51,56 +53,114 @@ private:
 public:
     DoublyLinkedList() : head(nullptr), tail(nullptr), current(nullptr) {
         listSize = 0;
+        currentPosition = 0;
         std::cout << "!!!!!ListCreated" ;
     }
 
     virtual ~DoublyLinkedList() {
-        go(First);
-        while(isValid()) {
+        while(!isEmpty()) {
             del(1);
         }
     }
 
-    void add(T value) {
+    void add(ListPosition here, T value) {
+        if ((here == Next)or(here == Back)) { return;}
         Node<T>* newNode = new Node<T>(value);
         if (!head) {
             head = newNode;
             tail = newNode;
+            listSize++; return;
         }
-        else {
+        if (here == Last) {
             newNode->previous = tail;
             tail->next = newNode;
             tail = newNode;
+            listSize++; return;
         }
+
+        if (here == First) {
+            newNode->next = head;
+            head->previous = newNode;
+            head = newNode;
+            currentPosition++;
+            listSize++; return;
+        }
+
+    }
+
+    void add(int thisPosition, T value) {
+        if (thisPosition < 1) { return; }
+        if (thisPosition > listSize + 1) { return; }
+        if (thisPosition == 1) { add(First, value); return; }
+        if (thisPosition == listSize + 1) { add(Last, value); return; }
+
+        go(thisPosition);
+        Node<T>* newNode = new Node<T>(value);
+        newNode->next = current;
+        newNode->previous = current->previous;
+
+        current->previous->next = newNode;
+        current->previous = newNode;
+
+        current = current->previous;
         listSize++;
 
     }
 
     void del(int toDelete) {
-        go(First);
-        int counter = 1;
-        while (isValid()) {
-            if (counter == toDelete) {
-                deleteOperation();
-                return;
-            }
-            go(Next);
-            counter++;
-        }
-        return;
+        if (toDelete < 1 && toDelete > listSize) { return; }
+        go(toDelete);
+        if (!isValid()) { return;}
+        if (position() != toDelete) { return;}
+        deleteOperation();
     }
 
     void go(ListPosition here) {
-        if (here == First) {current = head;}
-        if (here == Last) {current = tail;}
-        if (!isValid()) {return;};
-        if (here == Back) {current = current->previous;}
-        if (here == Next) {current = current->next;}
+        if (here == First) {current = head; currentPosition = 1; }
+        if (here == Last) {current = tail; currentPosition = listSize; }
+        if (!isValid()) {return;}
+        if (here == Back) {current = current->previous; currentPosition--; }
+        if (here == Next) {current = current->next; currentPosition++; }
+    }
+
+    void go(int thisPosition) {
+
+        if (isEmpty()) { return; }
+        if (thisPosition < 1) { return; }
+        if (thisPosition > listSize) { return; }
+        if (thisPosition == position()) { return; }
+
+        if (!isValid()) { go(First); }
+        if (thisPosition == 1) { go(First); return; }
+        if (thisPosition == listSize) { go(Last); return; }
+
+        int ToThere = abs(thisPosition - position());
+        int ToFirst = abs(thisPosition - 1);
+        int ToLast = abs(thisPosition - listSize);
+
+        if (ToFirst < ToThere && ToFirst < ToLast) { go(First);}
+        if (ToLast < ToThere && ToLast < ToFirst) { go(Last); }
+
+        while (thisPosition != position()) {
+            if (position() < thisPosition) { go(Next); }
+            if (position() > thisPosition) { go(Back); }
+        }
+
     }
 
     T getItem() {
         if (!isValid()) { return dummy; }
         return current->data;
+    }
+
+    void setItem(T toThis) {
+        if (!isValid()) { return; }
+        current->data = toThis;
+    }
+
+    int position() {
+        if (!isValid()) { return 0; }
+        return currentPosition;
     }
 
     bool isValid() {
@@ -111,7 +171,8 @@ public:
         return listSize;
     }
 
+    bool isEmpty(){
+        return listSize < 1;
+    }
+
 };
-
-
-
